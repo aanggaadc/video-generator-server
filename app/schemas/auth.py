fec9f2c560bcd1cc3e00310fac77b7900464789b
anggara setiawan <aanggaadc@gmail.com>
2026-05-09 20:24:13 +0700

    feat: create auth schema

---
+++ app/schemas/auth.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, EmailStr
+
+
+class RegisterRequest(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    id: str
+    name: str
+    email: EmailStr
+
+class AuthResponse(BaseModel):
+    token: str
+    user: UserResponse
