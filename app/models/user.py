618f80c5d644fe69e8fb8e9d3b2db03ca0f9abc8
anggara setiawan <aanggaadc@gmail.com>
2026-05-16 11:24:32 +0700

    refactor: change user id to UUID

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,14 @@
-from sqlalchemy import Column, String, TIMESTAMP, func
+import uuid
+
+from sqlalchemy import (
+    Column,
+    String,
+    Boolean,
+    TIMESTAMP,
+    func
+)
+
+from sqlalchemy.dialects.postgresql import UUID
 
 from app.core.database import Base
 
@@ -6,11 +16,45 @@ from app.core.database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(String, primary_key=True)
-    name = Column(String, nullable=False)
-    email = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4
+    )
+
+    name = Column(
+        String,
+        nullable=False
+    )
+
+    email = Column(
+        String,
+        unique=True,
+        nullable=False
+    )
+
+    password = Column(
+        String,
+        nullable=False
+    )
+
+    status = Column(
+        Boolean,
+        default=True
+    )
+
+    email_verified_at = Column(
+        TIMESTAMP,
+        nullable=True
+    )
+
     created_at = Column(
         TIMESTAMP,
         server_default=func.now()
+    )
+
+    updated_at = Column(
+        TIMESTAMP,
+        server_default=func.now(),
+        onupdate=func.now()
     )
